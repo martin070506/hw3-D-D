73784c631c8d6475e93c3cb48b095f88aaaf5f32
malin <malinkimg@gmail.com>
2025-06-08 23:39:44 +0300

    just a starting version of building a board from TXT file, still no actual game or level selection logic

--- src/BoardLogic/GameTile.java
+++ src/BoardLogic/GameTile.java
@@ -1,4 +1,25 @@
 package BoardLogic;
 
 public class GameTile {
+
+    private char CharIndicator;
+
+
+    public GameTile(char C)
+    {
+        this.CharIndicator=C;
+    }
+
+
+
+    @Override
+    public String toString()
+    {
+        return String.valueOf(this.CharIndicator);
+    }
+
+    public char GetTile()
+    {
+        return this.CharIndicator;
+    }
 }
